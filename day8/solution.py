7458e91c2e22e47122158048e3c3a5f43bc6a624
baaij <alex.bijo@gmail.com>
2020-12-19 18:36:15 +0100

    day 8 complete

--- day8/solution.py
+++ day8/solution.py
@@ -3,6 +3,22 @@ def getLinesForDay(day):
         lines = f.read().splitlines()
         return lines
 
+def parse(line):
+    instructionSplit = line.split()
+    cmd = instructionSplit[0]
+    incr = int(instructionSplit[1])
+    return [cmd, incr]
+
+def execute(cmd, incr, pointer, accumulator):
+    if cmd == 'acc':
+        pointer += 1
+        accumulator += incr
+    elif cmd == 'jmp':
+        pointer += incr
+    else:
+        pointer += 1
+    return [pointer, accumulator]
+
 def solve1():
     instructions = getLinesForDay(8)
     accumulator = 0
@@ -10,16 +26,54 @@ def solve1():
     pastIds = {}
     while pastIds.get(pointer) == None:
         pastIds[pointer] = ''
-        instrSplit = instructions[pointer].split()
-        cmd = instrSplit[0]
-        incr = int(instrSplit[1])
-        if cmd == 'acc':
-            pointer += 1
-            accumulator += incr
-        elif cmd == 'jmp':
-            pointer += incr
-        else:
-            pointer += 1
-    print(f'Accumulator value = {accumulator}')
+        parseResult = parse(instructions[pointer])
+        execResult = execute(parseResult[0], parseResult[1], pointer, accumulator)
+        pointer = execResult[0]
+        accumulator = execResult[1]
+        
+    print(f'Accumulator value for puzzle 1 = {accumulator}')
 
 solve1()
+
+#############################################
+
+def terminateAndGetResult(instructions):
+    isTerminating = True
+    pointer = 0
+    accumulator = 0
+    pastIds = {}
+    
+    while pointer < len(instructions):
+        if pastIds.get(pointer) != None:
+            isTerminating = False
+            break
+        pastIds[pointer] = ''
+        parseResult = parse(instructions[pointer])
+        execResult = execute(parseResult[0], parseResult[1], pointer, accumulator)
+        pointer = execResult[0]
+        accumulator = execResult[1]
+    return [pointer, accumulator] if isTerminating else None
+
+def flip(instruction):
+    flippedValue = None
+    if instruction.find('jmp') > -1:
+        flippedValue = instruction.replace('jmp', 'nop')
+    if instruction.find('nop') > -1:
+        flippedValue = instruction.replace('nop', 'jmp')
+    return flippedValue
+
+def solve2():
+    instructions = getLinesForDay(8)
+    for i in range(len(instructions)):
+        instruction = instructions[i]
+        flippedInstr = flip(instruction)
+        if flippedInstr:
+            instructions[i] = flippedInstr
+            result = terminateAndGetResult(instructions)
+            if result != None:
+                print(f'Accumulator value for puzzle 2 = {result[1]}')
+                break
+            instructions[i] = instruction
+
+
+solve2()
